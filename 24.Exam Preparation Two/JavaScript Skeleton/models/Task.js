ea21a753ffa0a077ead1f829ecfe04ded3d5ed07
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-07 17:30:29 +0300

    Exam Preparation Two

--- 24.Exam Preparation Two/JavaScript Skeleton/models/Task.js
+++ 24.Exam Preparation Two/JavaScript Skeleton/models/Task.js
@@ -5,11 +5,6 @@ let taskSchema = mongoose.Schema({
     status: {type:'string', required:'true'}
 });
 
-// DOBAVQME IME NA KOLEKCIATA T.E. TABLICATA
 let Task = mongoose.model('Task', taskSchema, 'allTasks');
 
 module.exports = Task;
-
-//SEGA TRQBVA DA OTDEM V ROBOMONGO I DA SI NAPRAVIM BAZATA SUS KOLEKCIQTA
-//ZA BAZATA VZEMI IMETO OT config.js FAILA, VIJ POSLEDNOTO NA connectionString:
-//A KOLEKCIQTA VECHE Q KRUSTIHME allTasks
